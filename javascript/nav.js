5993192a1eea63930ffad4e22984d162a7486c6a
Chris <chrishughallen@gmail.com>
2023-02-24 09:56:18 +0100

    converted to SPA

--- javascript/nav.js
+++ javascript/nav.js
@@ -3,17 +3,23 @@ const open_nav = () => {
   nav.classList.add("show-mobile-nav")
   document.body.classList.add("mobile-menu-open")
 }
+
 const close_nav = () => {
   nav.classList.remove("show-mobile-nav")
   document.body.classList.remove("mobile-menu-open")
 }
 
 // elements
-nav = document.querySelector(".navbar")
-open_nav_button = document.getElementById("open-nav")
-close_nav_button = document.getElementById("close-nav")
+let nav = document.querySelector(".navbar")
+let open_nav_button = document.getElementById("open-nav")
+let close_nav_button = document.getElementById("close-nav")
+let links = Array.from(document.querySelectorAll('nav ul li'))
+
+
 
 // listeners
 open_nav_button.addEventListener("click", open_nav)
 close_nav_button.addEventListener("click", close_nav)
-
+links.forEach((link) => {
+  link.addEventListener("click", close_nav)
+})
